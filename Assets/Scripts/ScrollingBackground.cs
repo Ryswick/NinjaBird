2ce5b8af9541aa81032c5998fb16419aab1f1785
Ryswick <bahamutheroes@yahoo.fr>
2014-10-06 22:10:42 +0200

    Added a scrolling background
    Now the background is scrolling in the main menu and in the game scenes

---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBackground : MonoBehaviour {
+
+	float scrollingSpeed = 1.5f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * scrollingSpeed * Time.deltaTime);
+
+		if(transform.position.x < -8.8f)
+			transform.position = new Vector3(15.2f, 0f, 1f);
+	}
+}
